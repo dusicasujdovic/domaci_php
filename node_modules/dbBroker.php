3d2b85d8f38441368422982707de08f0bb134357
dusicasujdovic <dusicasujdovic00@gmail.com>
2023-08-30 18:09:37 +0200

    Kreirana baza i konekcija sa bazom

---
+++ node_modules/dbBroker.php
@@ -0,0 +1,15 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "bioskop";
+
+global $mysqli;
+$mysqli= new mysqli($server, $user, $password, $database);
+if ($mysqli->error) {
+    printf("Connection failed: %s\n", $mysqli->error);
+    exit();
+}
+$mysqli->set_charset("utf8");
+
+?>
